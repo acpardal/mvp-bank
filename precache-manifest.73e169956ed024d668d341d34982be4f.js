a6da3b04148d1b2870d5afe0f4b96022c55e31bd
Andre Pardal <andre.pardal@devoteam.com>
2020-01-11 01:56:05 +0000

    first commit

---
+++ precache-manifest.73e169956ed024d668d341d34982be4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2b0619a4504ee9863ae9c9d33c62f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ac2fed8bf5039006850",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c5b78fe669a887aa4fab",
+    "url": "/static/js/2.eebd198c.chunk.js"
+  },
+  {
+    "revision": "2ac2fed8bf5039006850",
+    "url": "/static/js/main.4f006112.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
